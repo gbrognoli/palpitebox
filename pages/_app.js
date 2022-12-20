9028c138e302fa0fb0ec7e6680e5a59386f5e8b2
gbrognoli <gbrognoli53@gmail.com>
2022-12-20 16:24:53 -0300

    Segundo Commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import '../css/styles.css'
-import Layout from '../components/Layout'
+
 
 const MyApp = ({ Component, pageProps }) => {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+      <Component {...pageProps} />    
   )
 }
 export default MyApp
